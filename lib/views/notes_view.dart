d1ec85ffb7792083ffb0d9f0d8a5e24650c67ea8
eslam <abdelkadereslam53@gmail.com>
2025-02-03 02:18:20 +0200

    add font awesome flutter and appbar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widget/custom_app_bar.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 22.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 70,
+            ),
+            CustomAppBar(),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widget/custom_app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 40,
+          ),
+        ),
+        Spacer(),
+        Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            color: Colors.white.withValues(alpha: .3),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(
+            child: IconButton(
+              onPressed: () {},
+              icon: FaIcon(
+                FontAwesomeIcons.magnifyingGlass,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
