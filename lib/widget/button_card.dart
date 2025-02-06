0b14b7c846c9d6bd3f99b6084335b640569aafa8
eslam <abdelkadereslam53@gmail.com>
2025-02-06 23:15:00 +0200

    button to make note

--- lib/widget/button_card.dart
+++ lib/widget/button_card.dart
@@ -8,34 +8,55 @@ class ButtonCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 20),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hintText: 'Title',
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              minLines: 6,
+              maxLines: 19,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hintText: 'Your task',
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.blue,
                 borderRadius: BorderRadius.circular(10),
               ),
-              hintText: 'Title',
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          TextField(
-            minLines: 6,
-            maxLines: 19,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
               ),
-              hintText: 'Your task',
             ),
-          ),
-          
-        ],
+          ],
+        ),
       ),
     );
   }
