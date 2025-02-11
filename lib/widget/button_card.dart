ee26074f5af696bfc91d6167e76ea76e4232b2bf
eslam <abdelkadereslam53@gmail.com>
2025-02-11 14:10:37 +0200

    take input and validate data

--- lib/widget/button_card.dart
+++ lib/widget/button_card.dart
@@ -1,60 +1,99 @@
 import 'package:flutter/material.dart';
 
-class ButtonCard extends StatelessWidget {
-  const ButtonCard({super.key});
+class ButtonCard extends StatefulWidget {
+  ButtonCard({super.key});
 
+  @override
+  State<ButtonCard> createState() => _ButtonCardState();
+}
+
+class _ButtonCardState extends State<ButtonCard> {
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? title, content;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 20),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                onSaved: (value) {
+                  title = value;
+                },
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Filed is Required';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: 'Title',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                onSaved: (value) {
+                  content = value;
+                },
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Filed is Required';
+                  } else {
+                    return null;
+                  }
+                },
+                minLines: 6,
+                maxLines: 19,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: 'Your task',
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
                   borderRadius: BorderRadius.circular(10),
                 ),
-                hintText: 'Title',
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              minLines: 6,
-              maxLines: 19,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                hintText: 'Your task',
-              ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Add',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
+                child: TextButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                    } else {
+                      setState(() {
+                        autovalidateMode = AutovalidateMode.always;
+                      });
+                    }
+                  },
+                  child: Text(
+                    'Add',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
